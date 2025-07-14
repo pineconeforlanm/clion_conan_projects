92aa37d09afff3b59c7821c79eb922a645b45103
lanmeng <pineconeforlanm@outlook.com>
2025-07-14 18:44:53 +0800

    feat: add conan test grpc,boost,logger/spdlog .

---
+++ grpc/client/client.cc
@@ -0,0 +1,10 @@
+//
+// Created by lanm on 2025/7/14.
+//
+#include <iostream>
+
+auto main(int argc, char** argv) -> int {
+    std::cout << "hello grpc client";
+    return 0;
+}
+
---
+++ grpc/service/service.cc
@@ -0,0 +1,9 @@
+//
+// Created by lanm on 2025/7/14.
+//
+#include <iostream>
+
+auto main(int argc, char** argv) -> int {
+    std::println("hello grpc client");
+    return 0;
+}
