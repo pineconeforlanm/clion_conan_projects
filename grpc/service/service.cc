1994f459f58bb6c2eb617f081fc45bf7787d2bd0
lanmeng <pineconeforlanm@outlook.com>
2025-07-16 10:02:36 +0800

    feat: add grpc example

--- grpc/service/service.cc
+++ grpc/service/service.cc
@@ -2,8 +2,40 @@
 // Created by lanm on 2025/7/14.
 //
 #include <iostream>
+#include <memory>
+#include <string>
+#include <grpcpp/grpcpp.h>
+#include "hello.grpc.pb.h"
 
-auto main(int argc, char** argv) -> int {
-    std::println("hello grpc client");
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using example::ExampleService;
+using example::HelloRequest;
+using example::HelloReply;
+
+class ExampleServiceImpl final : public ExampleService::Service {
+    Status SayHello(ServerContext* context, const HelloRequest* request, HelloReply* reply) override {
+        std::string prefix("Hello ");
+        reply->set_message(prefix + request->name());
+        return Status::OK;
+    }
+};
+
+void RunServer() {
+    std::string server_address("0.0.0.0:50051");
+    ExampleServiceImpl service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+    server->Wait();
+}
+
+int main(int argc, char** argv) {
+    RunServer();
     return 0;
 }
